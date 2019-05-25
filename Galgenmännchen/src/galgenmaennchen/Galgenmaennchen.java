46eae2f70cfddd591bc0e25e19fc733ef417fe6c
adamcel <celina.adam@gmx.net>
2019-05-25 16:02:01 +0200

    Start game from another class

--- Galgenmännchen/src/galgenmaennchen/Galgenmaennchen.java
+++ Galgenmännchen/src/galgenmaennchen/Galgenmaennchen.java
@@ -6,12 +6,7 @@ import java.util.Scanner;
 
 public class Galgenmaennchen {
 	
-	public static void main(String[] args) {
-		Galgenmaennchen game = new Galgenmaennchen();
-		game.startGame();
-	}
-	
-	private void startGame() {
+	public void startGame() {
 		Scanner scan = new Scanner(System.in);
 
 		String word = start(scan);
---
+++ Galgenmännchen/src/galgenmaennchen/Main.java
@@ -0,0 +1,10 @@
+package galgenmaennchen;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Galgenmaennchen game = new Galgenmaennchen();
+		game.startGame();
+	}
+
+}
