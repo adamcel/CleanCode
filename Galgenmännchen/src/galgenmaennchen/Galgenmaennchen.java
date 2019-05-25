4ee5aa84675bcfea2b26ec4add34533cfe714110
adamcel <celina.adam@gmx.net>
2019-05-25 15:58:23 +0200

    Extract 5 methods and remove break statements
    extracted methods:
    start, startGame, explainGame, garbleWord and checkIfNewLetter

--- Galgenmännchen/src/galgenmaennchen/Galgenmaennchen.java
+++ Galgenmännchen/src/galgenmaennchen/Galgenmaennchen.java
@@ -5,72 +5,50 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Galgenmaennchen {
-
-	// main method of game
+	
 	public static void main(String[] args) {
-
-		System.out.println("Welcome to the game 'Galgenmaennchen'!");
-
-		System.out.println("Please let the game host enter a word first:");
-
-		// Scanner for the user input
+		Galgenmaennchen game = new Galgenmaennchen();
+		game.startGame();
+	}
+	
+	private void startGame() {
 		Scanner scan = new Scanner(System.in);
-		String word = scan.nextLine().toLowerCase();
 
-		System.out.println("Please enter each round a letter of your choice. Try to guess the word!\n(If you type in more than a letter then the first letter will be used.)\n");
-
-		// Every round the user gets the output, what parts of the word he already guessed and how many mistrials are left until he looses.
-		System.out.println("Letter\t\tOutput\t\tLeft mistrials");
+		String word = start(scan);
+		explainGame();
 
 		boolean correctWord = false;
 		int leftMistrials = 10;
 		char letter;
 		List<String> letters = new ArrayList<>();
-		String guessedWord = "";
+		String guessedWord = garbleWord(word);
 
-		for (int i = 0; i < word.length(); i++) {
-			if (word.charAt(i) == ' ') {
-				guessedWord += word.charAt(i);
-			} else {
-				guessedWord += "-";							// For every letter a minus is added to the string.
-			}
-		}
-
-		while (!correctWord) {
+		while (!correctWord && leftMistrials > 0) {
 			letter = scan.next().toLowerCase().charAt(0);
 			String newGuessedWord = "";
 
-			// Starting on -1 because else this part will never be entered.
-			for (int i = -1; i < letters.size(); i++) {
-				if (i != -1 && letters.get(i).equals(letter + "")) {
-					System.out.println("You already entered this letter!");
-					break;
+			// When the letter is entered the first time, then do this part.
+			if (checkIfNewLetter(letters, letter)) {
+				letters.add(letter + "");
+				// Checking if the letter was correct and change the guessed word accordingly.
+				for (int j = 0; j < word.length(); j++) {
+					if (letter == word.charAt(j)) {
+						newGuessedWord += letter;
+					} else {
+						newGuessedWord += guessedWord.charAt(j);
+					}
+				}
+
+				// Calculating the left mistrials
+				if (guessedWord.equals(newGuessedWord)) {
+					leftMistrials--;
 				}
 				
-				// When the letter is entered the first time, then do this part.
-				if (i == letters.size() - 1) {
-					letters.add(letter + "");
-					// Checking if the letter was correct and change the guessed word accordingly.
-					for (int j = 0; j < word.length(); j++) {
-						if (letter == word.charAt(j)) {
-							newGuessedWord += letter;
-						} else {
-							newGuessedWord += guessedWord.charAt(j);
-						}
-					}
-					
-					// Calculating the left mistrials
-					if (guessedWord.equals(newGuessedWord)) {
-						leftMistrials--;
-						if (leftMistrials == 0) {
-							System.out.println("\t\t" + guessedWord + "\t\t" + leftMistrials);
-							System.out.println("I'm sorry but you couldn't guess the right word. The host wins!");
-							break;
-						}
-					}
-
+				if (leftMistrials == 0) {
+					System.out.println("\t\t" + guessedWord + "\t\t" + leftMistrials);
+					System.out.println("I'm sorry but you couldn't guess the right word. The host wins!");
+				} else {
 					guessedWord = newGuessedWord;
-
 					System.out.println("\t\t" + guessedWord + "\t\t" + leftMistrials);
 
 					// Guessed the word?
@@ -78,14 +56,53 @@ public class Galgenmaennchen {
 						correctWord = true;
 						System.out.println("Congrats! You guessed the right word!");
 					}
-					break;
 				}
+
 			}
-
+			
 		}
-
 		scan.close();
+	}
+	
+	private String start(Scanner scan) {
+		System.out.println("Welcome to the game 'Galgenmaennchen'!");
 
+		System.out.println("Please let the game host enter a word first:");
+
+		// Scanner for the user input
+		String word = scan.nextLine().toLowerCase();
+		return word;
+	}
+	
+	private void explainGame() {
+		System.out.println("Please enter each round a letter of your choice. Try to guess the word!\n(If you type in more than a letter then the first letter will be used.)\n");
+
+		// Every round the user gets the output, what parts of the word he already guessed and how many mistrials are left until he looses.
+		System.out.println("Letter\t\tOutput\t\tLeft mistrials");
+	}
+	
+	private String garbleWord(String word) {
+		String guessedWord = "";
+		
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == ' ') {
+				guessedWord += word.charAt(i);
+			} else {
+				guessedWord += "-";							// For every letter a minus is added to the string.
+			}
+		}
+		
+		return guessedWord;
+	}
+	
+	private boolean checkIfNewLetter(List<String> letters, char c) {
+		for (int i = 0; i < letters.size(); i++) {
+			if (letters.get(i).equals(c + "")) {
+				System.out.println("You already entered this letter!");
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
